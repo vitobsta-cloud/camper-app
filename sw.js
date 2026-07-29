9809e15d0dfd3fa8c1e2bebd136961e3224b98ff
vitobsta-cloud <vitobsta@gmail.com>
2026-07-29 12:04:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'camper-app-v3';
+const CACHE_NAME = 'camper-app-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
